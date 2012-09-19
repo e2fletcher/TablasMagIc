d1f08b47efe6a5d9158acfbacbdcec920e5dd71c
Ender Fletcher <e2fletcher@gmail.com>
2012-09-19 11:51:02 -0430

    -help

--- Main.cc
+++ Main.cc
@@ -2,15 +2,24 @@
 
 #include <iostream>
 #include <stdlib.h>
+#include <cstring>
 
 using namespace std;
 
+void help(){
+	cout << "usage: gtablas <numero de tablas>\n";
+}
+
 int main(int argc, char** argv){
+	if(argc < 2 || strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0){
+		help();
+		return 0;
+	}
+
 	Tabla t(atoi(argv[1]));
 
 	for(int i = 1; i <= t.Quantite(); ++i){
 		cout << t.Print(i) << endl;
 	}
-
 	return 0;
 } 
